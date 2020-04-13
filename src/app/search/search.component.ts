adb1885f36aa8fc7add69254d03c640cdb02ebfd
Dan Njoroge <greatdaniels@gmail.com>
2020-04-13 11:39:36 +0300

    adding search function definition to search.component.ts

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-search',
@@ -6,6 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
+
+  searchFor: string;
+
+  @Output () searchResult = new EventEmitter<any>()
+
+  search() {
+    this.searchResult.emit(this.searchFor);
+    this.searchFor = "";
+  }
 
   constructor() { }
 
