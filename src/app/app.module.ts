755db38a1c2ce91b903bf6f38976c70fa6379f23
Dan Njoroge <greatdaniels@gmail.com>
2020-04-13 11:17:09 +0300

    adding the repo component with html structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
+import { RepoComponent } from './repo/repo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent
+    UserComponent,
+    RepoComponent
   ],
   imports: [
     BrowserModule,
