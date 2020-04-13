f5769a4e4fd4c73a6e85b35686a028d70db120cf
Dan Njoroge <greatdaniels@gmail.com>
2020-04-13 19:31:23 +0300

    adding the routing to app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
+import { RepoComponent } from './repo/repo.component';
+import { UserComponent } from './user/user.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"user", component:UserComponent},
+  {path:"repo", component:RepoComponent},
+  {path:"", redirectTo:"/user",pathMatch:"full"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
